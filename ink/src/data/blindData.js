145d1ad60ff186b6c75db534475b95be8a4864c9
Melvin Vermast <143947718+Smulvin@users.noreply.github.com>
2026-05-27 10:17:05 +0200

    deaf setup

---
+++ ink/src/data/blindData.js
@@ -0,0 +1,52 @@
+export const blindItems = [
+  {
+    id: 1,
+    image: '/images/apple.png',
+    answer: 'Apple',
+  },
+  {
+    id: 2,
+    image: '/images/banana.png',
+    answer: 'Banana',
+  },
+  {
+    id: 3,
+    image: '/images/car.png',
+    answer: 'Car',
+  },
+  {
+    id: 4,
+    image: '/images/dog.png',
+    answer: 'Dog',
+  },
+  {
+    id: 5,
+    image: '/images/cat.png',
+    answer: 'Cat',
+  },
+  {
+    id: 6,
+    image: '/images/train.png',
+    answer: 'Train',
+  },
+  {
+    id: 7,
+    image: '/images/tree.png',
+    answer: 'Tree',
+  },
+  {
+    id: 8,
+    image: '/images/house.png',
+    answer: 'House',
+  },
+  {
+    id: 9,
+    image: '/images/bike.png',
+    answer: 'Bike',
+  },
+  {
+    id: 10,
+    image: '/images/boat.png',
+    answer: 'Boat',
+  },
+];
