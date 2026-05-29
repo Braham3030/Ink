33a82cf66dba8bdfbe286a1464732472f9ad1f4c
Melvin Vermast <143947718+Smulvin@users.noreply.github.com>
2026-05-29 12:21:55 +0200

    fixed image names in data

--- ink/src/data/deafData.js
+++ ink/src/data/deafData.js
@@ -1,52 +1,52 @@
 export const blindItems = [
   {
     id: 1,
-    image: '/images/minigames/deaf/1.png',
+    image: '/images/minigames/deaf/eat.png',
     answer: 'Eten',
   },
   {
     id: 2,
-    image: '/images/minigames/deaf/2.png',
+    image: '/images/minigames/deaf/thanks.png',
     answer: 'Bedankt',
   },
   {
     id: 3,
-    image: '/images/minigames/deaf/3.png',
+    image: '/images/minigames/deaf/sorry.png',
     answer: 'Sorry',
   },
   {
     id: 4,
-    image: '/images/minigames/deaf/4.png',
+    image: '/images/minigames/deaf/please.png',
     answer: 'Alsjeblieft',
   },
   {
     id: 5,
-    image: '/images/minigames/deaf/5.png',
+    image: '/images/minigames/deaf/yes.png',
     answer: 'Ja',
   },
   {
     id: 6,
-    image: '/images/minigames/deaf/6.png',
+    image: '/images/minigames/deaf/no.png',
     answer: 'Nee',
   },
   {
     id: 7,
-    image: '/images/minigames/deaf/7.png',
+    image: '/images/minigames/deaf/bathroom.png',
     answer: 'Badkamer',
   },
   {
     id: 8,
-    image: '/images/minigames/deaf/8.png',
+    image: '/images/minigames/deaf/done.png',
     answer: 'Klaar',
   },
   {
     id: 9,
-    image: '/images/minigames/deaf/9.png',
+    image: '/images/minigames/deaf/drink.png',
     answer: 'Drinken',
   },
   {
     id: 10,
-    image: './images/minigames/deaf/10.png',
+    image: './images/minigames/deaf/stop.png',
     answer: 'Stoppen',
   },
 ];
