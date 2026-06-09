889947ba070cee1b4fa2f7404bc0e8f6b7cbe4e6
Braham <152489943+Braham3030@users.noreply.github.com>
2026-06-09 13:35:51 +0200

    chore: add @vite-pwa/astro dependency to package.json

--- ink/astro.config.mjs
+++ ink/astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import node from '@astrojs/node';
+import astroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,8 +13,31 @@ export default defineConfig({
     prefetchAll: true,
     defaultStrategy: 'viewport'
   },
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
-});
+  // Integrations
+  integrations: [
+    astroPWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        // With globPatterns, the given file types will be cached by the service worker in the browser.
+        globPatterns: ['**/*.{js,css,html,jpg,svg,astro,webp}'],
+        maximumFileSizeToCacheInBytes: 20000000, // 10 MB
+      },
+      manifest: {
+        name: 'Ink - Disability Awareness Game',
+        short_name: 'Ink',
+        description: 'A game to raise awareness about disabilities.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+  });
