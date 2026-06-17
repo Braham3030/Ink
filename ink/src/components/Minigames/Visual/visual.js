d8975af90710d713b9111bfe378d23e855373b6e
jadmi <justin.admiraal@hva.nl>
2026-06-17 10:18:52 +0200

    sources added

--- ink/src/components/Minigames/Visual/visual.js
+++ ink/src/components/Minigames/Visual/visual.js
@@ -4,6 +4,8 @@ window.helpContext = {
   text: "Zorg ervoor dat je zo weinig mogelijk aandacht brengt aan het uiterlijk. Mensen willen zich ondanks hun uiterlijk niet voelen alsof ze anders zijn dan de rest.",
 };
 
+// Claude voor gebruik optional chaining (?.) bij popup, voorkomt script error als hij niet inlaad.
+
 import { levels } from "../../../data/visualData.js";
 
 /* =========================
